865a62e65d6a76ab1984c8054207f10cf0ea5dfc
Rahime Yildiz <rahime1yildiz@gmail.com>
2022-09-14 21:56:12 +0200

    added assessment2

--- assessment2/main.js
+++ assessment2/main.js
@@ -1,2 +1,12 @@
-import { encryptText } from './modules/encrypt.js';
+import { encryptText } from "./modules/encrypt.js";
 
+document.getElementById("shift").onchange = function (e) {
+  let shift = e.target.value;
+  console.log(shift);
+
+  let text = document.getElementById("plaintext").value;
+
+  let total = encryptText(text, shift);
+
+  document.getElementById("encryptedtext").value = total;
+};
