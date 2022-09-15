3e781d647b2f09f0560df3edc2e9a68604888549
Rahime Yildiz <rahime1yildiz@gmail.com>
2022-09-15 14:47:55 +0200

    fetch the questions

--- assessment3/modules/question.api.js
+++ assessment3/modules/question.api.js
@@ -1,6 +1,14 @@
 const fetchQuestion = () => {
-    // Fetch a question from thehttps://the-trivia-api.com/api/questions?limit=5 API
-    return {};    
-}
+  // Fetch a question from the https://the-trivia-api.com/api/questions?limit=5 API
+
+  fetch("https://the-trivia-api.com/api/questions?limit=5")
+    .then((response) => response.json())
+    .then((data) => console.log(data))
+    .catch((err) => {
+      console.log(err);
+    });
+
+  return {};
+};
 
 export { fetchQuestion };
