e588013adff735078114b105d23028c1dcc712b9
Lis <lis@MacBook-Pro.local>
2022-09-17 15:44:30 +0200

    Multiples

--- assessment1/index.js
+++ assessment1/index.js
@@ -1,5 +1,11 @@
 function sumOfMultiple(limit) {
-    // Your code goes here
+   let sum = 0;
+   for(let i = 0; i < limit; i++){
+    if(i % 3 === 0 || i % 5 === 0 ){
+        sum += i; 
+    }
+   }
+   return sum;
 }
 
 module.exports = sumOfMultiple;
